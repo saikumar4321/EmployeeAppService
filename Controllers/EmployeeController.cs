1b8e1f054e611647bf03881f319daf0d18f166c8
Saikumar Giri <Saikumar Giri@EMPINHYL-047>
2025-04-30 11:13:51 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,98 @@
+﻿using EmployeeApp.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeApp.Model.Request;
+using EmployeeApp.DAL.IRepositories;
+using Newtonsoft.Json;
+
+
+namespace EmployeeAppService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        private readonly IEmployee _employeeController;
+
+        private readonly ILogger<EmployeeController> _logger;
+        public EmployeeController(IEmployee employee, ILogger<EmployeeController> logger)
+        {
+
+            _employeeController = employee;
+            _logger = logger;
+        }
+
+
+        [HttpPost("AddEmployee")]
+        public IActionResult AddEmployee([FromBody] EmployeeData employee)
+        {
+            _logger.LogInformation("AddEmployee" + JsonConvert.SerializeObject(employee));
+            if (employee == null)
+            {
+                return BadRequest("Employee not Available");
+            }
+            _employeeController.AddEmployee(employee);
+
+            return Ok(employee);
+
+            //return CreatedAtRoute("Get",
+
+            //    new {id=  customer.Id},customer);
+        }
+
+
+        [HttpGet("GetEmployee")]
+
+        public IActionResult GetEmployee()
+        {
+            IEnumerable<EmployeeData> customers = _employeeController.GetEmployee();
+
+            return Ok(customers);
+        }
+
+
+        [HttpGet("GetEmployeeByID")]
+        public IActionResult GetEmployeeByID(int EMPId)
+        {
+            EmployeeData employee = _employeeController.GetEmployeeById(EMPId);
+            if (employee == null)
+            {
+                return NotFound("Customer Can't Found");
+            }
+            return Ok(employee);
+        }
+
+        [HttpPut("UpdateEmployee")]
+        public IActionResult UpdateEmployee(int EMPId, [FromBody] EmployeeData employee)
+        {
+            if (employee == null)
+            {
+                return BadRequest("Customer Not Available");
+
+            }
+            EmployeeData employeetoupdate = _employeeController.GetEmployeeById(EMPId);
+            if (employeetoupdate == null)
+            {
+                return BadRequest("Customer Not Updated");
+            }
+            _employeeController.UpdateEmployee(employeetoupdate, employee);
+            return Ok(employee);
+
+        }
+
+        [HttpDelete("DeleteEmployee")]
+        public IActionResult DeleteEmployee(int EMPId)
+        {
+            EmployeeData employee = _employeeController.GetEmployeeById(EMPId);
+            if (employee == null)
+            {
+                return NotFound("Data Not Found");
+            }
+            _employeeController.DeleteEmployee(employee);
+            return Ok(employee);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+using EmployeeApp.DAL.Configurations;
+using EmployeeApp.DAL.EmployeeDBContext;
+using EmployeeApp.DAL.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using System.IO;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var logFolderPath = Path.Combine("D:", "Microservices_Samples\\EmployeeAppService\\LogFiles"); // ? You can customize the path
+
+// Create folder if not exists
+if (!Directory.Exists(logFolderPath))
+{
+    Directory.CreateDirectory(logFolderPath);
+}
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File(
+        Path.Combine(logFolderPath, "log-.txt"), // ? Save in your folder
+        rollingInterval: RollingInterval.Day,    // ? New file every day
+        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information
+    )
+    .CreateLogger();
+
+// Add Serilog to ASP.NET Core logging
+builder.Host.UseSerilog();
+
+builder.Services.AddDbContext<EmployeeContext>(
+    opts => opts.UseSqlServer(builder.Configuration["ConnectionStrings:CRUDEmployee"])
+    );
+
+builder.Services.AddScoped<IEmployee , EmployeeConfigurations>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
